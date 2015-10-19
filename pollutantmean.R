d87007c958633a99ffaa21ec50d03ba878c9c303
Álvaro Suárez Tovar <asuarezt@gmail.com>
2015-10-19 17:40:00 +0200

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,12 +1,11 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        data <- data.frame();
-        files <- list.files(directory, full.names = TRUE);
+        files_full <- list.files(directory, full.names = TRUE) 
         
-        for (index in files) {
-                data <- rbind(data, read.csv(index, comment.char = ""))
+        dat <- data.frame()
+        
+        for (i in id) {
+                dat <- rbind(dat, read.csv(files_full[i]))
         }
         
-        neededMonitors <- subset(data, ID %in% id);
-        pollutantMean <- mean(neededMonitors[[pollutant]], na.rm = TRUE);
-        pollutantMean;
+        mean(dat[, pollutant], na.rm = TRUE)
 }
